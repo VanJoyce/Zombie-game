d6c7fb957f763419f0e2b46a73a8206d9afbd52a
jneo0003 <jneo0003@student.monash.edu>
2020-05-04 21:51:48 +0800

    New version

--- src/game/ZombieLimbs.java
+++ src/game/ZombieLimbs.java
@@ -3,15 +3,15 @@ package game;
 import java.util.ArrayList;
 
 public class ZombieLimbs {
-	private String[] zombieLimbs= {"rightHand","leftHand","rightLeg","leftLeg"};
+	private ArrayList<String> zombieLimbs= {"rightHand","leftHand","rightLeg","leftLeg"};
 	private int noOfHands=2;
 	private int noOfLegs=2;
 	private int noOfLimbs=4;
 	
 	public void loseLimbs() {
 		int limbIndex=(int)Math.random()*((noOfLimbs)-0)+0;
-		String limb=zombieLimbs[limbIndex];
-		zombieLimbs=ArrayUtils.remove(zombieLimbs, 2);
+		String limb=zombieLimbs.get(limbIndex);
+		zombieLimbs.remove(limbIndex);
 		if(limb.substring(limb.length()-4, limb.length()-1).equals("Hand")){
 			noOfHands-=1;
 			noOfLimbs-=1;
