5451823e4751d401348d3ce9fc958015987dad39
jneo0003 <jneo0003@student.monash.edu>
2020-05-18 18:05:49 +0800

    Forgotten classes pushed

---
+++ src/game/ZombieClubs.java
@@ -0,0 +1,12 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieClubs extends WeaponItem{
+
+	public ZombieClubs(String name, char displayChar, int damage, String verb) {
+		super(name, displayChar, damage, verb);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/game/ZombieMaze.java
@@ -0,0 +1,12 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieMaze extends WeaponItem {
+
+	public ZombieMaze(String name, char displayChar, int damage, String verb) {
+		super(name, displayChar, damage, verb);
+		// TODO Auto-generated constructor stub
+	}
+
+}
